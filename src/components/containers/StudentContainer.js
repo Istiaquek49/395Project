a9fd9e435fc7a072bdee111e63591b3efd8d872c
Istiaque Khan <Istiaque.Khan49@myhunter.cuny.edu>
2021-07-06 22:55:06 -0400

    Added classes and maps to props

--- src/components/containers/StudentContainer.js
+++ src/components/containers/StudentContainer.js
@@ -1,7 +1,34 @@
-const StudentContainer = () => {
-  return (
-    <h1>Single Student View</h1>
-  );
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStudentThunk } from "../../store/thunks";
+import { StudentsView }  from "../views";
+
+class StudentContainer extends Component {
+  componentDidMount() {
+    this.props.fetchStudent(this.props.match.params.id);//getting campus ID from url
+  }
+
+  render() {
+    return (
+      <StudentsView 
+        student={this.props.student}
+      />
+    );
+  }
+}
+
+// map state to props
+const mapState = (state) => {
+  return {
+    student: state.student,
+  };
 };
 
-export default StudentContainer;
+// map dispatch to props
+const mapDispatch = (dispatch) => {
+  return {
+    fetchStudent: (id) => dispatch(fetchStudentThunk(id)),
+  };
+};
+
+export default connect(mapState, mapDispatch)(StudentContainer);

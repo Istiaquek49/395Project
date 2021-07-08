8b9ca1d87353f75c6a2cc9da23eaef425d8cab57
Istiaque Khan <Istiaque.Khan49@myhunter.cuny.edu>
2021-07-07 21:33:39 -0400

    Added the import given from homepageview

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -1,3 +1,9 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
